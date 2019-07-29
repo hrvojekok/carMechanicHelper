388d11b56a43050c41db0dffe041af637383017e
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-07-29 22:49:47 +0200

    added views for every specific mechanic

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -17,34 +17,47 @@
                      <?php
                      //TODO continue here
                      $user = Auth::user();
-                     print($user->id);
-                     $customer = DB::table('items')->where('mechanic_id', $user)->pluck('id');
-                     $sizeCustomer = sizeof($customer);
-                     print($sizeCustomer);
-                     for($j=0; $j<$sizeCustomer; $j++){
-                       print($customer[$j]);
-                     }
+                     //print($user->id);
+                     // print("<br><br>");
+                     $userID = $user->id;
+                     // print($userID);
+                     // print("<br><br>");
 
+                     //$descriptionReal = DB::table('items')->where('mechanic_id', $userID)->pluck('description');
+                     //print($descriptionReal);
+                     //print("<br><br>");
 
+                     // $customer = DB::table('items')->where('mechanic_id', $user)->pluck('id');
+                     // $sizeCustomer = sizeof($customer);
+                     // //print("sizeCustomer");
+                     // //print($sizeCustomer);
+                     // for($j=0; $j<$sizeCustomer; $j++){
+                     //   print($customer[$j]);
+                     // }
 
-                       $make = DB::table('items')->pluck('make');
-                       $model = DB::table('items')->pluck('model');
-                       $engine = DB::table('items')->pluck('engine');
-                       $description = DB::table('items')->pluck('description');
+                       $make = DB::table('items')->where('mechanic_id', $userID)->pluck('make');
+                       $model = DB::table('items')->where('mechanic_id', $userID)->pluck('model');
+                       $engine = DB::table('items')->where('mechanic_id', $userID)->pluck('engine');
+                       $description = DB::table('items')->where('mechanic_id', $userID)->pluck('description');
                        $user_id = DB::table('items')->pluck('user_id');
 
                        $size = sizeof($make);
                        //print($tasks);
-                       if($description != ""){
+                       if($make != ""){
                          for($i=0; $i<$size; $i++){
+                             print("Marka vozila: ");
                              print($make[$i]);
                              print("  ||  ");
+                             print("Model vozila: ");
                              print($model[$i]);
                              print("  ||  ");
+                             print("Motor: ");
                              print($engine[$i]);
                              print("  ||  ");
+                             print("Opis kvara: ");
                              print($description[$i]);
                              print("  ||  ");
+                             print("Ime naručitelja: ");
                              $userName = DB::table('users')->where('id', $user_id[$i])->pluck('name');
                              $userNameSubstring = substr($userName, 2, -2);
                              print($userNameSubstring);
