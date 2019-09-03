62c9f29b2a7b0809792bb8525cad9a67f3cee8a5
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-09-03 16:31:25 +0200

    update

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dobro došli</div>
+                <div class="card-header">Popis dogovorenih poslova</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Uspješno ste prijavljeni.
+
                     <br><br>
 
                     <?php
